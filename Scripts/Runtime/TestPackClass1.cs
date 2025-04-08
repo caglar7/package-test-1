f65c0b71b733ced8863a3b5ed882c1c43467c44b
Caglar <60190180+caglar7@users.noreply.github.com>
2025-04-08 17:22:00 +0300

    Initial commit

---
+++ Scripts/Runtime/TestPackClass1.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace MyTools.TestPackage1.Scripts
+{
+    public static class TestPackClass1
+    {
+        public static void TestMethod()
+        {
+            Debug.Log("TestPackClass1.TestMethod"); 
+        }
+    }
+}
